ba245dcbfe2954287f1f9381827b3957fa2d78fd
Ben.Ashdown <ben99944@gmail.com>
2020-08-18 09:18:21 +1000

    Initial Commit

---
+++ src/main/java/Rever/PillX/PillXApplication.java
@@ -0,0 +1,13 @@
+package Rever.PillX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PillXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Rever/PillX/PillXApplicationTests.java
@@ -0,0 +1,13 @@
+package Rever.PillX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PillXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
