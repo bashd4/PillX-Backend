a2c021a3cebf370268bfcf3325aacc6a66ccffc9
B-Ashdown <ben99944@gmail.com>
2020-08-18 12:47:04 +1000

    Expose first endpoint

--- src/main/java/Rever/PillX/PillXApplication.java
+++ src/main/java/Rever/PillX/PillXApplication.java
@@ -2,12 +2,19 @@ package Rever.PillX;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PillXApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PillXApplication.class, args);
 	}
 
+	@RequestMapping(value = "/")
+	public String hello() {
+		return "Hello, World!";
+	}
 }
